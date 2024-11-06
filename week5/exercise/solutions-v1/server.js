33951bb48f7f073e781d9c287452f479ebca9beb
Diana.K <diana.kosashka@gmail.com>
2024-11-06 20:39:27 +0200

    Week 5 : solutions 3&4

--- week5/exercise/solutions-v1/server.js
+++ week5/exercise/solutions-v1/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const https = require("https");
 const url = require("url");
 const fs = require("fs");
 const path = require("path");
@@ -7,36 +8,44 @@ const serverErrorHandler = (err) => {
   console.error(err);
 };
 
-/** Exercise 1 */
+/** Exercise 1 & 4 */
 const getRequestHandler = (req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const reqPath = parsedUrl.pathname;
-  const match = reqPath.match(/^\/fetchTemplate\/(.+)$/);
+  const matchFetchTemplate = reqPath.match(/^\/fetchTemplate\/(.+)$/); // Example URL : http://localhost:3000/fetchTemplate/home?name=John%20Doe
+  const matchLoadProp = reqPath.match(/^\/loadProp\/(.+)$/); // Example URL : http://localhost:3000/loadProp/name
 
-  if (!match) {
+  if (!matchFetchTemplate && !matchLoadProp) {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.write("GET request");
     res.end();
-  } else {
-    const templateName = match[1];
+  } else if (matchFetchTemplate) {
+    /** Exercise 1 */
+    const templateName = matchFetchTemplate[1];
     if (templateName === "home") {
       fetchTemplateHandler(templateName, parsedUrl.query, res);
     }
+  } else if (matchLoadProp) {
+    /** Exercise 4 */
+    const propName = matchLoadProp[1];
+    loadPropHandler(propName, res);
   }
 };
 
-/** Exercise 2 */
+/** Exercise 2 & 3 */
 const postRequestHandler = (req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const reqPath = parsedUrl.pathname;
-  const match = reqPath.match(/^\/fetchTemplate\/(.+)$/);
+  const matchFetchTemplate = reqPath.match(/^\/fetchTemplate\/(.+)$/); // Example URL : http://localhost:3000/fetchTemplate/home
+  const matchFetchWebsite = reqPath.match(/^\/fetchWebsite\/(.+)$/); // Example URL : http://localhost:3000/fetchWebsite/reddit OR http://localhost:3000/fetchWebsite/www.reddit.com/.rss
 
-  if (!match) {
+  if (!matchFetchTemplate && !matchFetchWebsite) {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.write("Not Found");
     res.end();
     return;
-  } else {
+  } else if (matchFetchTemplate) {
+    /** Exercise 2 */
     let bodyBuffer = "";
     let bodyParams = {};
     req.on("error", serverErrorHandler);
@@ -46,10 +55,29 @@ const postRequestHandler = (req, res) => {
     req.on("end", function () {
       bodyParams = JSON.parse(bodyBuffer);
 
-      const templateName = match[1];
+      const templateName = matchFetchTemplate[1];
       if (templateName === "home") {
         fetchTemplateHandler(templateName, bodyParams, res);
       }
+    });
+  } else if (matchFetchWebsite) {
+    /** Exercise 3 */
+    const websiteName = matchFetchWebsite[1];
+    const isFullUrl = websiteName.includes("www");
+    const fullUrl = isFullUrl
+      ? `https://${websiteName}`
+      : `https://www.${websiteName}.com`;
+    https.get(fullUrl, (response) => {
+      let body = "";
+      response.on("error", serverErrorHandler);
+      response.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      response.on("end", () => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(body);
+        res.end();
+      });
     });
   }
 };
@@ -76,6 +104,20 @@ const fetchTemplateHandler = (templateName, replaceValues, res) => {
     res.write(filledTemplate);
     res.end();
   });
+};
+
+const props = {
+  name: "John Doe",
+  age: "30",
+  city: "New York",
+};
+const loadPropHandler = (propName, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  Object.keys(props).includes(propName)
+    ? res.write(props[propName])
+    : res.write("Property not found");
+
+  res.end();
 };
 
 /** HTTP server */
