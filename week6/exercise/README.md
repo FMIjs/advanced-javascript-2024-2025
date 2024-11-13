e8ef854ceb1a9657f845d347f930539108dda678
Diana.K <diana.kosashka@gmail.com>
2024-11-13 16:42:51 +0200

    Add exercise 1 in week 6

---
+++ week6/exercise/README.md
@@ -0,0 +1,11 @@
+# Упражнение 6
+
+## 13.11.24
+
+1.  Създайте итератор, който изполва стуктурата `alphabet-tree` и изписва английската азбука .
+
+```javascript
+for (const letter of alphabetTree) {
+  console.log(letter); // Outputs A, B, C, ... Z
+}
+```
---
+++ week6/exercise/alphabet-tree.js
@@ -0,0 +1,61 @@
+const alphabetTree = {
+  value: "M",
+  left: {
+    value: "G",
+    left: {
+      value: "D",
+      left: {
+        value: "B",
+        left: { value: "A", left: null, right: null },
+        right: { value: "C", left: null, right: null },
+      },
+      right: {
+        value: "F",
+        left: { value: "E", left: null, right: null },
+        right: null,
+      },
+    },
+    right: {
+      value: "J",
+      left: {
+        value: "H",
+        left: null,
+        right: { value: "I", left: null, right: null },
+      },
+      right: {
+        value: "L",
+        left: { value: "K", left: null, right: null },
+        right: null,
+      },
+    },
+  },
+  right: {
+    value: "T",
+    left: {
+      value: "Q",
+      left: {
+        value: "O",
+        left: { value: "N", left: null, right: null },
+        right: { value: "P", left: null, right: null },
+      },
+      right: {
+        value: "S",
+        left: { value: "R", left: null, right: null },
+        right: null,
+      },
+    },
+    right: {
+      value: "W",
+      left: {
+        value: "U",
+        left: null,
+        right: { value: "V", left: null, right: null },
+      },
+      right: {
+        value: "Y",
+        left: { value: "X", left: null, right: null },
+        right: { value: "Z", left: null, right: null },
+      },
+    },
+  },
+};
