4bb67ab6f84488d20b9969fbc74f192a29b7b3cc
Diana.K <diana.kosashka@gmail.com>
2024-12-11 20:19:44 +0200

    Add delete functionality in week10 solution

--- week10/exercise/index.js
+++ week10/exercise/index.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/pages'));
 
 app.use(express.static(path.resolve('static')));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Parse form data
 app.use(express.static('public')); // Serve static files from the 'public' folder 
 
@@ -17,7 +18,11 @@ app.get('/', (req, res) => {
     res.render('index', { tasks });
 });
 
-app.post('/add-task', (req, res) => {
+app.get('/task/add', (req, res) => {
+  res.render('add-new-task',);
+});
+
+app.post('/task/add', (req, res) => {
     const task = req.body.task;
     if (task) {
         tasks.push(task); 
@@ -25,6 +30,16 @@ app.post('/add-task', (req, res) => {
     res.redirect('/');
 });
 
+app.delete('/task', (req, res) => {
+  const task = req.body.task;
+  if (task) {
+    const index = tasks.indexOf(task);
+    if (index !== -1) {
+      tasks.splice(index, 1);
+    }
+  }
+  res.status(200).send();
+});
 
 app.listen(8080, function () {
   console.log('Server is listening on :8080');
